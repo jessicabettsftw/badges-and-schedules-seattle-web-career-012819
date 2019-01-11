53a9316846e1abd1f36d8a2b74761bbc9bed0186
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-11 02:56:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,14 @@ def badge_maker(name)
   badge = "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
+def batch_badge_creator(names_array)
+  badge_messages_array = []
   
+  array.each do |element|
+    message = badge_maker(element)
+    badge_messages_array.push(message)
+  end
+  return badge_messages_array
 end
 
 def assign_rooms()
