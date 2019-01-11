ab92246f6cc0c87d64fc1d089de5c922a9d3ea2f
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-11 03:03:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,11 @@ def batch_badge_creator(names_array)
 end
 
 def assign_rooms(speakers_array)
+  room_assignment = []
   
   speakers_array.each do |speaker|
     room = speakers_array.index_of(speaker)
     message = "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room_assignment.push(message)
+  end
 end
